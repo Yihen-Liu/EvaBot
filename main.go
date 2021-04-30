2ce1f2bf7fd259334f887acd9f7cc861df31024a
Yihen <liuyihen@gmail.com>
2021-04-30 14:59:53 +0800

    .

--- main.go
+++ main.go
@@ -6,13 +6,14 @@
 package main
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/riversgo007/EvaBot/core"
 	"log"
 	"time"
 )
 
 func main() {
-	bot, err := core.NewBotAPI("MyAwesomeBotToken")
+	bot, err := core.NewBotAPI("1706227378:AAE1i8R1CgpG_WJd7wr6y_bned_ggt0NJ7o")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -31,6 +32,7 @@ func main() {
 	time.Sleep(time.Millisecond * 500)
 	updates.Clear()
 
+	hasGiven:=make(map[string]string)
 	for update := range updates {
 		if update.Message == nil {
 			continue
@@ -51,23 +53,23 @@ func main() {
 			case "withArgument":
 				msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
 			case "url":
+				if given, ok:=hasGiven[update.Message.From.UserName]; ok{
+					msg.Text = fmt.Sprintf("%s has been DONE, url:%s, don't repeated.",update.Message.From, given)
+					break
+				}
 				response, err:=bot.Request(core.NewCreateChatInviteLink(update.Message.Chat.ID))
 				if err!=nil{
 					log.Println("")
-					continue
+					break
 				}
 
-				data, err:=response.Result.MarshalJSON()
-
-				var resp struct {
-					Result *core.ChatInviteLink
-				}
-				if err := json.Unmarshal(data, &resp); err != nil {
-					continue
+				var inviteLink core.ChatInviteLink
+				if err := json.Unmarshal(response.Result, &inviteLink); err != nil {
+					break
 				}
 
-
-				msg.Text = resp.Result.InviteLink
+				msg.Text = fmt.Sprintf("allocate to:%s, url:%s",update.Message.From, inviteLink.InviteLink)
+				hasGiven[update.Message.From.UserName] = inviteLink.InviteLink
 			default:
 				msg.Text = "I don't know that command"
 			}
