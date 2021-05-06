9fc9857fc28947e1ffbdc92243f6f379cc2c9b86
Yihen <liuyihen@gmail.com>
2021-05-06 16:03:38 +0800

    fix bug

--- main.go
+++ main.go
@@ -87,21 +87,25 @@ func main() {
 
 	for update := range updates {
 		go func() {
-			if update.ChatMember!=nil && update.ChatMember.InviteLink!=nil{
+			if update.ChatMember!=nil{
 				var invitor Invitor
-				err := DB.Where("group_name=? and user_id=? and invitor_url=?", update.ChatMember.Chat.Title, update.ChatMember.From.ID, update.ChatMember.InviteLink.InviteLink).First(&invitor).Error
-				if err==nil&&update.ChatMember.NewChatMember.Status=="member"&&update.ChatMember.OldChatMember.Status=="left"{
-					if _err := DB.Model(&invitor).Updates(Invitor{Status: 1,UpdateTime: time.Now().Unix()}).Error; _err != nil {
-						log.Error("for join the chat, save invitor err:",_err.Error())
+				err := DB.Where("group_name=? and user_id=?", update.ChatMember.Chat.Title, update.ChatMember.From.ID).First(&invitor).Error
+
+				if err == nil && update.ChatMember.NewChatMember.Status=="left" && update.ChatMember.OldChatMember.Status=="member"&&invitor.Status!=0{  // 离开群组，需要update status=0
+					if _err := DB.Model(&invitor).Updates(Invitor{Status: 0,UpdateTime: time.Now().Unix()}).Error; _err != nil {
+						log.Error("for left the chat, save invitor err:",_err.Error())
 					}
 					return
 				}
 
-				if err == nil && update.ChatMember.NewChatMember.Status=="left" && update.ChatMember.OldChatMember.Status=="member"{  // 离开群组，需要update status=0
-					invitor.Status = 0
-					invitor.UpdateTime = time.Now().Unix()
-					if _err := DB.Model(&invitor).Updates(Invitor{Status: 0,UpdateTime: time.Now().Unix()}).Error; _err != nil {
-						log.Error("for left the chat, save invitor err:",_err.Error())
+			}
+			if update.ChatMember!=nil && update.ChatMember.InviteLink!=nil{
+				var invitor Invitor
+				err := DB.Where("group_name=? and user_id=? and invitor_url=?", update.ChatMember.Chat.Title, update.ChatMember.From.ID, update.ChatMember.InviteLink.InviteLink).First(&invitor).Error
+
+				if err==nil&&update.ChatMember.NewChatMember.Status=="member"&&update.ChatMember.OldChatMember.Status=="left"&&invitor.Status!=1{
+					if _err := DB.Model(&invitor).Updates(Invitor{Status: 1,UpdateTime: time.Now().Unix()}).Error; _err != nil {
+						log.Error("for join the chat, save invitor err:",_err.Error())
 					}
 					return
 				}
