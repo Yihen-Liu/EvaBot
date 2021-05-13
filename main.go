c663850153370cf302aae6649c66126faedd416f
Yihen <liuyihen@gmail.com>
2021-05-13 10:06:03 +0800

    remove unused push bot

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ package main
 import (
 	"github.com/riversgo007/EvaBot/features/captcha"
 	"github.com/riversgo007/EvaBot/features/invite"
-	"github.com/riversgo007/EvaBot/features/newcoin"
+	//"github.com/riversgo007/EvaBot/features/newcoin"
 	"log"
 	"os"
 	"os/signal"
@@ -18,7 +18,7 @@ import (
 func main()  {
 	go invite.RunService()
 	go captcha.RunService()
-	go newcoin.RunService()
+	//go newcoin.RunService()
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 	<-signalChan
